a9be5fbe0848532f51ca1981ef8bc4e8a04fa301
Suraj-K-Dubey <surajdubey302@gmail.com>
2018-07-16 22:58:30 +0530

    Solution for Find Maximun Depth in XML2 from XML part of python module of HackerRank

---
+++ python/XML/Findd_Maximum_Depth.py
@@ -0,0 +1,50 @@
+'''
+You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as 0.
+
+Input Format:
+The first line contains N, the number of lines in the XML document. 
+The next N lines follow containing the XML document.
+
+Output Format:
+Output a single line, the integer value of the maximum level of nesting in the XML document.
+
+Sample Input:
+6
+<feed xml:lang='en'>
+    <title>HackerRank</title>
+    <subtitle lang='en'>Programming challenges</subtitle>
+    <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>
+    <updated>2013-12-25T12:00:00</updated>
+</feed>
+
+Sample Output:
+1
+
+Explanation:
+Here, the root is a feed tag, which has depth of 0. 
+The tags title, subtitle, link and updated all have a depth of 1.
+
+Thus, the maximum depth is 1.
+'''
+import xml.etree.ElementTree as etree
+
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    level += 1
+    if level >= maxdepth:
+        maxdepth = level
+    for child in elem:
+        depth(child, level)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
